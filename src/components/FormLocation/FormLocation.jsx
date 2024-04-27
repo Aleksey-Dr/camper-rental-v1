ecc44e01e70b75914a0610480c1bbb61d86f1803
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-04-27 11:33:11 +0300

    add placeholder in form location

--- src/components/FormLocation/FormLocation.jsx
+++ src/components/FormLocation/FormLocation.jsx
@@ -1,15 +1,24 @@
+import icons from 'images/icons.svg';
 
 import css from './FormLocation.module.scss';
 
 const FormLocation = () => {
     return (
         <form className={css.form}>
-            <label htmlFor='location' className={css.label}>Location</label>
-            <input
-                type="text"
-                id='location'
-                className={css.input}
-            />
+            <label htmlFor="location" className={css.label}>
+                Location
+            </label>
+            <div className={css['form-input-wrapper']}>
+                <input
+                    type="text"
+                    id="location"
+                    placeholder='Kyiv, Ukraine'
+                    className={css['form-input']}
+                />
+                <svg width="20" height="20" className={css['form-icon']}>
+                    <use href={`${icons}#icon-map-pin`}></use>
+                </svg>
+            </div>
         </form>
     );
 };
